ddbe53317cffd25098f3d1a0a91741387fb09d33
Raviraj Singh Sisodiya <99286116+RavirajSingh2147@users.noreply.github.com>
2022-10-06 15:15:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1= Math.floor(Math.random() * 6) + 1 ;
+var randomimagesource = "images/dice" + randomNumber1 + ".png";
+var image =document.querySelectorAll("img")[0].setAttribute("src", randomimagesource);
+
+var randomNumber2=Math.floor(Math.random()* 6 ) + 1;
+var randomimagesource2= "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomimagesource2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 WIN'S !"
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 WIN'S !"
+}
+else{
+    document.querySelector("h1").innerHTML="🎲DRAW !"
+}
